1c890b3e14ff3d95a20fa2e07ca122f4dd8dff17
Jack Knox <jackjackfilms1@gmail.com>
2025-10-24 14:13:20 +0100

    Add Manager class

---
+++ Strype-ScriptCore/Source/Strype/Room/Manager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Strype
+{
+    public class Manager
+    {
+        protected virtual void OnCreate() { }
+        protected virtual void OnUpdate(float ts) { }
+    }
+
+}
